5ccbe12c22bb7988e619f6dff1402acb8df47013
AntonRDev <anton.r.dev@gmail.com>
2016-09-15 08:05:15 +0300

    add opportunity to add paper into db

--- cmyk/paperwidget.cpp
+++ cmyk/paperwidget.cpp
@@ -1,6 +1,9 @@
 #include <QSqlTableModel>
 #include <QTableView>
 #include <QBoxLayout>
+#include <QPushButton>
+#include <QMessageBox>
+#include <QSqlError>
 #include "paperwidget.h"
 
 namespace CMYK {
@@ -9,9 +12,27 @@ PaperWidget::PaperWidget(QWidget *parent) : QWidget(parent){
 
     setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
 
-    QVBoxLayout *VBL = new QVBoxLayout(this);
+    auto vbl = new QVBoxLayout(this);
 
-    auto model = new QSqlTableModel();
+    // tools
+    {
+        auto hbl = new QHBoxLayout;
+
+        auto add_btn = new QPushButton;
+        add_btn->setText(tr("Добавить"));
+        hbl->addWidget(add_btn);
+        connect(add_btn, SIGNAL(clicked(bool)), this, SLOT(slot_add_paper()) );
+
+        auto del_btn = new QPushButton;
+        del_btn->setText(tr("Удалить"));
+        hbl->addWidget(del_btn);
+
+        hbl->addItem(new QSpacerItem(0, 0, QSizePolicy::Expanding, QSizePolicy::Minimum));
+
+        vbl->addLayout(hbl);
+    }
+
+    model = new QSqlTableModel();
     model->setTable("paper");
     model->select();
     model->setHeaderData(0, Qt::Horizontal, tr("Название"));
@@ -22,11 +43,56 @@ PaperWidget::PaperWidget(QWidget *parent) : QWidget(parent){
 
     QTableView *view = new QTableView();
     view->setModel(model);
-    //view->hideColumn(0); // don't show the name
     view->show();
 
-    VBL->addWidget(view);
+    vbl->addWidget(view);
 
+}
+
+void PaperWidget::slot_add_paper(){
+    int cnt=model->rowCount();
+
+        if (!model->insertRows(cnt,1))
+        {
+             QMessageBox::critical(this, tr("БД"),
+                                 tr("Вставка строки вызвала следующую ошибку:\r\n %1")
+                                 .arg(model->lastError().databaseText()));
+
+        }
+}
+
+void PaperWidget::slot_delete_paper(){
+    //ВАЖНО
+    //Проверить бумаги в списке заказов. Если используется -> удаление запрещено
+    //Добавить диалог ok / cansel
+
+    //ЧУЖОЙ КОД, ПОСМОТРЕТЬ ПОЗЖЕ
+//    int row;
+//      QItemSelectionModel    *selectModel;
+//      QModelIndexList         indexes;
+//      QModelIndex             index;
+
+//        // Узнаем выделенную строку
+//        selectModel = view->selectionModel();
+//        indexes = selectModel->selectedIndexes();
+
+//        foreach(index, indexes)
+//        {
+//            QString str;
+//            row = index.row();
+//            if (!model->removeRows(row,1))
+//            {
+//                str = model->lastError().text();
+//                qDebug() << str << "\n\r";
+//                break;
+//            }
+//            else
+//            {
+//                view->setRowHidden(row, true);
+//            }
+//        }
+//        SlotChange();
+//        SlotClick();
 }
 
 } // namespace CMYK
--- cmyk/paperwidget.h
+++ cmyk/paperwidget.h
@@ -2,6 +2,7 @@
 #define PAPERWIDGET_H
 
 #include <QWidget>
+#include <QSqlTableModel>
 
 namespace CMYK {
 
@@ -9,9 +10,15 @@ class PaperWidget : public QWidget{
 
     Q_OBJECT
 
+private:
+    QSqlTableModel * model;
+
 public:
     PaperWidget(QWidget *parent = 0);
 
+private slots:
+    void slot_add_paper();
+    void slot_delete_paper();
 };
 
 } // namespace CMYK
